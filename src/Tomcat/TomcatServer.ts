c6b6018d7db67dad47ef02a91b4e410865156a66
Summer <victoriasqq@gmail.com>
2018-01-19 11:51:46 +0800

    fix #59: only restart server when switch between debug and run (#78)

--- src/Tomcat/TomcatServer.ts
+++ src/Tomcat/TomcatServer.ts
@@ -1,19 +1,42 @@
 'use strict';
 
 import * as path from "path";
+import * as vscode from "vscode";
 import { Utility } from "../Utility";
 
 export class TomcatServer {
+    public needRestart: boolean = false;
     private _name: string;
     private _tomcatPath: string;
     private _started: boolean;
     private _extensionPath: string;
+    private _debugPort: number;
+    private _debugWorkspace: vscode.WorkspaceFolder;
+    private _isDebugging: boolean = false;
 
     constructor(name: string, tomcatPath: string, extensionPath: string) {
         this._name = name.trim();
         this._tomcatPath = tomcatPath.trim();
         this._started = false;
         this._extensionPath = extensionPath.trim();
+    }
+
+    public setDebugInfo(debugging: boolean, port: number, workspace: vscode.WorkspaceFolder): void {
+        this._isDebugging = debugging;
+        this._debugPort = port;
+        this._debugWorkspace = workspace;
+    }
+
+    public getDebugPort(): number {
+        return this._debugPort;
+    }
+
+    public getDebugWorkspace(): vscode.WorkspaceFolder {
+        return this._debugWorkspace;
+    }
+
+    public isDebugging(): boolean {
+        return this._isDebugging;
     }
 
     public setStarted(started: boolean): void {
