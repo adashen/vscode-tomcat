6d09f867c4c1c243bd13053bef911e70d5f8ee58
Qingqing Sun <qisun@microsoft.com>
2018-02-07 10:04:25 +0800

    update more info url

--- src/Constants.ts
+++ src/Constants.ts
@@ -19,7 +19,7 @@ export const PORT: string = 'port';
 export const INVALID_SERVER_DIRECTORY: string = 'Please make sure you select a valid Tomcat Directory.';
 
 // tslint:disable-next-line:no-http-string
-export const UNABLE_SHUTDOWN_URL: string = 'http://tomcat.10.x6.nabble.com/Unable-to-shutdown-Tomcat-td5012627.html';
+export const UNABLE_SHUTDOWN_URL: string = 'https://stackoverflow.com/questions/36566401/severe-could-not-contact-localhost8005-tomcat-may-not-be-running-error-while/48636631#48636631';
 
 export const RESTART_CONFIG_ID: string = 'restart_when_http(s)_port_change';
 
